0c84eb52e4ae4240d7eac18f50dff7dcf963e8b0
Yubvin <Yubvin@gmail.com>
2017-02-21 23:59:26 +0200

    Добавлена функции поиска, добавление изображения.

--- public_html/controller.js
+++ public_html/controller.js
@@ -1,20 +1,70 @@
 var catApp = angular.module("CatsWeb", ['ngResource']);
 
-catApp.config(['$httpProvider', function ($httpProvider) {    
-	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+catApp.directive('fileModel', [ '$parse', function($parse) {
+	return {
+		restrict : 'A',
+		link : function(scope, element, attrs) {
+			var model = $parse(attrs.fileModel);
+			var modelSetter = model.assign;
+
+			element.bind('change', function() {
+				scope.$apply(function() {
+					modelSetter(scope, element[0].files[0]);
+				});
+			});
+		}
+	};
+} ]);
+catApp.service('fileUpload', ['$http', function($http) {
+	this.uploadFileToUrl = function(uploadUrl, name, age, breed, file, cats) {
+		var fd = new FormData();
+		fd.append('name', name);
+		fd.append('age', age);
+		fd.append('breed', breed);
+                fd.append('image', file);
+		$http.post(uploadUrl, fd, {
+			transformRequest : angular.identity,
+			headers : {
+				'Content-Type' : undefined
+			}
+		}).success(function(response) {
+                    cats[cats.length] = response;
+		}).error(function() {
+		});
+	};
+        
+        this.updateImgById = function(uploadUrl, id, file, cat) {
+		var fd = new FormData();
+		fd.append('id', id);
+                fd.append('image', file);
+		$http.post(uploadUrl, fd, {
+			transformRequest : angular.identity,
+			headers : {
+				'Content-Type' : undefined
+			}
+		}).success(function(response) {
+                    cat.imgName = response.imgName;
+		}).error(function() {
+		});
+	};
 }]);
 
-catApp.controller("CatsCntrl", function($scope, $window, $http) {
+catApp.controller("CatsCntrl", function($scope, $http, fileUpload) {
     
+    $scope.query = {};
+    $scope.queryBy = '$';
+    $scope.cats = [];
     $scope.editingData = {};
+    $scope.boxAddCat = false;
+    $scope.boxSearchCat = false;
     
     $http.get("http://localhost:8080/rest/cat/").then(function (response) {
       $scope.cats = response.data;
-      for (var i = 0, length = $scope.tabelsData.length; i < length; i++) {
+      for (var i = 0, length = $scope.cats.length; i < length; i++) {
         $scope.editingData[$scope.cats[i].id] = false;
       }
     });
-
+    
     $scope.modify = function(cat){
         $scope.editingData[cat.id] = true;
     };
@@ -24,7 +74,7 @@ catApp.controller("CatsCntrl", function($scope, $window, $http) {
         $scope.editingData[cat.id] = false;
     };
     
-    $scope.update = function(cat){
+    $scope.update = function(cat, imgCatUp){
         $scope.editingData[cat.id] = false;
         
         $http.put("http://localhost:8080/rest/cat/update", cat).
@@ -33,26 +83,31 @@ catApp.controller("CatsCntrl", function($scope, $window, $http) {
         error(function(data) {
             alert( "failure message: " + JSON.stringify({data: data}));
         });
+        var file = imgCatUp;
+        var uploadUrl = "http://localhost:8080/rest/cat/imgUpdate";
+        fileUpload.updateImgById(uploadUrl, cat.id, file, cat);   
+        
         
     };
-    $scope.addRow = function(){		
+    $scope.addCat = function(){		
 
-        var data = 'name=' + $scope.name + '&age=' + $scope.age+ '&breed=' + $scope.breed;
-
-        $http.post("http://localhost:8080/rest/cat/add", data).
-        success(function(data) {
-            $scope.cats[$scope.cats.length] = data;
-            $scope.editingData[data.id] = false;
-        }).
-        error(function(data) {
-            alert( "failure message: " + JSON.stringify({data: data}));
-        });
-            
+        var name = $scope.name;
+	var age = $scope.age;
+        var breed = $scope.breed;
+        var file = $scope.imgCat;
+	var uploadUrl = "http://localhost:8080/rest/cat/add";
+	fileUpload.uploadFileToUrl(uploadUrl, name, age, breed, file, $scope.cats);
+        
         $scope.name='';
         $scope.age='';
         $scope.breed='';
+        
     }; 
-    
+    $scope.searchCancel = function(){
+        $scope.query = {};
+        $scope.strict = null;
+    };
+   
     $scope.removeRow = function(id, $index){
         
         var url = "http://localhost:8080/rest/cat/delete/" + id;
@@ -60,10 +115,18 @@ catApp.controller("CatsCntrl", function($scope, $window, $http) {
         $http.delete(url).
         success(function(){
             $scope.cats.splice($index, 1);
-            //$window.location.reload();
         }).error(function(data){
            alert("failure message: " + JSON.stringify({data : data}));
         });
     };
+    
+    $scope.SearchCatBox = function(){
+        $scope.boxSearchCat=!$scope.boxSearchCat;
+        if($scope.boxSearchCat === false){
+            $scope.query = {};
+            $scope.strict = null;
+        }
+    };
+    
+    
 });
-
